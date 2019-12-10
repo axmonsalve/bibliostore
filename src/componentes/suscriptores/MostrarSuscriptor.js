63ef8ee1338f1be12e4bb9491b01f0ec10c334b5
axel <axel@correo.com>
2019-12-10 20:33:47 -0300

    Mostrando un suscriptogit add .

--- src/componentes/suscriptores/MostrarSuscriptor.js
+++ src/componentes/suscriptores/MostrarSuscriptor.js
@@ -1,28 +1,64 @@
-import React from 'react'
+import React from "react";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { Link } from "react-router-dom";
 import Spinner from "../layout/spinner";
+import PropTypes from 'prop-types';
 
+const MostrarSuscriptor = ({ suscriptor }) => {
+  if (!suscriptor) return <Spinner />;
+  return (
+    <div className="row my-4">
+      <div className="col-md-6 ">
+        <Link to="/suscriptores" className="btn btn-secondary">
+          <i className="fas fa-arrow-circle-left"></i> Volver al listado
+        </Link>
+      </div>
 
-const MostrarSuscriptor = () => {
-    return ( 
-        <h1>Mostrar Suscriptor...</h1>
-     );
+      <div className="col-md-6">
+        <Link
+          to={`/suscriptores/editar/${suscriptor.id}`}
+          className="btn btn-primary float-right"
+        >
+          <i className="fas fa-pencil-alt"></i> Editar Suscriptor
+        </Link>
+      </div>
+      
+      <hr className="mx-5 w-100"/>
+      
+      <div className="col-12">
+          <p className="mb-4 h2">
+              {suscriptor.nombre} {suscriptor.apellido}
+          </p>
+          <p className="h5">
+              <span className="font-weight-bold ">Carrera: </span>
+              {suscriptor.carrera}
+          </p>
+          <p className="h5">
+              <span className="font-weight-bold">Código: </span>
+              {suscriptor.codigo}
+          </p>
+      </div>
+    </div>
+  );
+};
+
+MostrarSuscriptor.propTypes = {
+    firestore : PropTypes.object.isRequired 
 }
- 
+
 export default compose(
-    firestoreConnect(props => [
-        {
-            collection : 'suscriptores',
-            //Para no perder la referencia del state de suscriptores
-            storeAs : 'suscriptor',
-            //Para saber que registro me traigo de la bd
-            doc : props.match.params.id
-        }
-    ]),
-    connect(({firestore : {ordered}}, props)=>({
-        suscriptor : ordered.suscriptor && ordered.suscriptor[0]
-    }))
+  firestoreConnect(props => [
+    {
+      collection: "suscriptores",
+      //Para no perder la referencia del state de suscriptores
+      storeAs: "suscriptor",
+      //Para saber que registro me traigo de la bd
+      doc: props.match.params.id
+    }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    suscriptor: ordered.suscriptor && ordered.suscriptor[0]
+  }))
 )(MostrarSuscriptor);
--- src/componentes/suscriptores/NuevoSuscriptor.js
+++ src/componentes/suscriptores/NuevoSuscriptor.js
@@ -1,53 +1,55 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import {firestoreConnect} from 'react-redux-firebase';
-import PropTypes from 'prop-types';
+import { firestoreConnect } from "react-redux-firebase";
+import PropTypes from "prop-types";
 
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 class NuevoSuscriptor extends Component {
   state = {
-      nombre: '',
-      apellido: '',
-      carrera: '',
-      codigo: ''
+    nombre: "",
+    apellido: "",
+    carrera: "",
+    codigo: ""
   };
 
   //Extraer valores del input para colocarlo en el state
   leerDato = e => {
     this.setState({
-        [e.target.name] : e.target.value
-    })
-  }
+      [e.target.name]: e.target.value
+    });
+  };
 
   //Agrega un nuevo suscriptor a la bd
   agregarSuscriptor = e => {
-      e.preventDefault();
+    e.preventDefault();
 
-      //Extraer valores del state
-        const nuevoSuscriptor = {...this.state}
+    //Extraer valores del state
+    const nuevoSuscriptor = { ...this.state };
 
-      //Extraer firestore de props
-      const {firestore, history} = this.props;
+    //Extraer firestore de props
+    const { firestore, history } = this.props;
 
-      //Guardar en la bd
-      firestore.add({
-          collection : 'suscriptores',
-
-      },nuevoSuscriptor)
-        .then(()=>{
-            Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: 'Your work has been saved',
-                showConfirmButton: false,
-                timer: 1500
-              })
-              history.push('/suscriptores');
-        })
-        .catch(error => console.log(error))
-  }
-
+    //Guardar en la bd
+    firestore
+      .add(
+        {
+          collection: "suscriptores"
+        },
+        nuevoSuscriptor
+      )
+      .then(() => {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your work has been saved",
+          showConfirmButton: false,
+          timer: 1500
+        });
+        history.push("/suscriptores");
+      })
+      .catch(error => console.log(error));
+  };
 
   render() {
     return (
@@ -62,68 +64,66 @@ class NuevoSuscriptor extends Component {
             <i className="fas fa-user-plus"></i> Nuevo Suscriptor
           </h2>
           <div className="row justify-content-center">
-              <div className="col-md-8 mt-8">
-                  <form
-                    onSubmit={this.agregarSuscriptor}
-                  >
-                      <div className="form-group">
-                          <label>Nombre:</label>
-                          <input 
-                            type="text"
-                            className="form-control"
-                            name="nombre"
-                            placeholder="Nombre del suscriptor"
-                            required
-                            onChange={this.leerDato}
-                            value={this.state.nombre}
-                            />
-                      </div>
+            <div className="col-md-8 mt-8">
+              <form onSubmit={this.agregarSuscriptor}>
+                <div className="form-group">
+                  <label>Nombre:</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="nombre"
+                    placeholder="Nombre del suscriptor"
+                    required
+                    onChange={this.leerDato}
+                    value={this.state.nombre}
+                  />
+                </div>
 
-                      <div className="form-group">
-                          <label>Apellido:</label>
-                          <input 
-                            type="text"
-                            className="form-control"
-                            name="apellido"
-                            placeholder="Apellido del suscriptor"
-                            required
-                            onChange={this.leerDato}
-                            value={this.state.apellido}
-                            />
-                      </div>
+                <div className="form-group">
+                  <label>Apellido:</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="apellido"
+                    placeholder="Apellido del suscriptor"
+                    required
+                    onChange={this.leerDato}
+                    value={this.state.apellido}
+                  />
+                </div>
 
-                      <div className="form-group">
-                          <label>Carrera:</label>
-                          <input 
-                            type="text"
-                            className="form-control"
-                            name="carrera"
-                            placeholder="Carrera del suscriptor"
-                            required
-                            onChange={this.leerDato}
-                            value={this.state.carrera}
-                            />
-                      </div>
+                <div className="form-group">
+                  <label>Carrera:</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="carrera"
+                    placeholder="Carrera del suscriptor"
+                    required
+                    onChange={this.leerDato}
+                    value={this.state.carrera}
+                  />
+                </div>
 
-                      <div className="form-group">
-                          <label>Código:</label>
-                          <input 
-                            type="text"
-                            className="form-control"
-                            name="codigo"
-                            placeholder="Código del suscriptor"
-                            required
-                            onChange={this.leerDato}
-                            value={this.state.codigo}
-                            />
-                      </div>
-                      <input 
-                        type="submit" 
-                        value="Agregar Suscriptor"
-                        className="btn btn-success btn-block"
-                        />
-                  </form>
-              </div>
+                <div className="form-group">
+                  <label>Código:</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="codigo"
+                    placeholder="Código del suscriptor"
+                    required
+                    onChange={this.leerDato}
+                    value={this.state.codigo}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Agregar Suscriptor"
+                  className="btn btn-success btn-block"
+                />
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -132,7 +132,7 @@ class NuevoSuscriptor extends Component {
 }
 
 NuevoSuscriptor.propTypes = {
-    firestore : PropTypes.object.isRequired
-}
+  firestore: PropTypes.object.isRequired
+};
 
 export default firestoreConnect()(NuevoSuscriptor);
