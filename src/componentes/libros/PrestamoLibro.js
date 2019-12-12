0dc9762512c03269fcf30f9ed00fa74deccfa0f1
axel <axel@correo.com>
2019-12-12 16:46:09 -0300

    Agregando la persona que solicita el libro a la BD

--- src/componentes/libros/PrestamoLibro.js
+++ src/componentes/libros/PrestamoLibro.js
@@ -5,12 +5,13 @@ import { firestoreConnect } from "react-redux-firebase";
 import { Link } from "react-router-dom";
 import Spinner from "../layout/spinner";
 import PropTypes from "prop-types";
+import FichaAlumno from "../suscriptores/FichaSuscriptor";
 import Swal from "sweetalert2";
 
 class PrestamoLibro extends Component {
   state = {
     noResultados: false,
-    busqueda: '',
+    busqueda: "",
     resultado: {}
   };
 
@@ -19,49 +20,100 @@ class PrestamoLibro extends Component {
     e.preventDefault();
 
     //obtener el valor a buscar
-    const {busqueda} = this.state;
+    const { busqueda } = this.state;
 
     //Extraer firestore
-    const {firestore} = this.props;
+    const { firestore } = this.props;
 
     //Hacer la consulta mediante un llamado del usuario
-    const coleccion = firestore.collection('suscriptores');
+    const coleccion = firestore.collection("suscriptores");
     const consulta = coleccion.where("codigo", "==", busqueda).get();
 
     //Leer los resultados
     consulta.then(resultado => {
-      if(resultado.empty){
+      if (resultado.empty) {
         //No hay resultados
         this.setState({
           noResultados: true,
-          resultado:{}
-        })
-      }else{
+          resultado: {}
+        });
+      } else {
         //Si hay resultados
         const datos = resultado.docs[0];
-         //Este resultado (datos) lo coloco en el state
-         this.setState({
-            resultado: datos.data(),
-            noResultados: false
-         })
+        //Este resultado (datos) lo coloco en el state
+        this.setState({
+          resultado: datos.data(),
+          noResultados: false
+        });
       }
-    })
-  }
+    });
+  };
 
+  //Almacena los datos del alumno para solicitar el libro
+  solicitarPrestamo = () => {
+    const suscriptor = this.state.resultado;
+
+    //Fecha de alta 
+    suscriptor.fecha_solicitud = new Date().toLocaleDateString();
+
+    //Obtener el libro
+    const {libro} = this.props;
+
+    //Agregar el suscriptor al libro
+    libro.prestados.push(suscriptor);
+
+    //Obtener firestore y history de props
+    const {firestore, history} = this.props;
+
+    //Almacenar en la BD
+    firestore.update({
+      collection: 'libros',
+      doc: libro.id
+    }, libro)
+      .then(
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `Libro solicitado correctamente`,
+          showConfirmButton: false,
+          timer: 1500
+        }),
+        history.push('/')
+      )
+  }
 
   //Almacenar el código en el state
   leerDato = e => {
     this.setState({
-      [e.target.name] : e.target.value
-    })
-  }
-
+      [e.target.name]: e.target.value
+    });
+  };
 
   render() {
     //Extraer el libro
     const { libro } = this.props;
 
     if (!libro) return <Spinner />;
+
+    //Extremos los datos del alumno que está en el state
+    const { noResultados, resultado } = this.state;
+
+    let fichaAlumno, btnSolicitar;
+    if (resultado.nombre) {
+      fichaAlumno = <FichaAlumno alumno={resultado} />;
+      btnSolicitar = (
+        <button
+          type="button"
+          className="btn btn-success btn-block"
+          onClick={this.solicitarPrestamo}
+        >
+          Solicitar prestamo
+        </button>
+      );
+    } else {
+      fichaAlumno = null;
+      btnSolicitar = null;
+    }
 
     return (
       <div className="row">
@@ -72,25 +124,41 @@ class PrestamoLibro extends Component {
         </div>
         <div className="col-12">
           <h2>
-            <i className="fas fa-book"></i> <span className="font-weight-bold">Solicitar prestamo:</span>  {libro.titulo}
+            <i className="fas fa-book"></i>{" "}
+            <span className="font-weight-bold">Solicitar prestamo:</span>{" "}
+            {libro.titulo}
           </h2>
-          <div className="row justify-content-center">
+          <div className="row justify-content-center mb-5">
             <div className="col-md-8">
-              <form
-                onSubmit={this.buscarAlumno}
-              >
-                <legend className="color-primary text-center mt-5">
+              <form onSubmit={this.buscarAlumno} className="mb-4">
+                <legend className="color-primary text-center mt-3">
                   Busca el suscriptor por código
                 </legend>
-                <div className="form-group">
-                  <input type="text"
-                    name="busqueda"
-                    className="form-control"
-                    onChange={this.leerDato}
-                  />
+
+                <div className="row">
+                  <div className="col-md-8">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        name="busqueda"
+                        className="form-control"
+                        onChange={this.leerDato}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <input
+                      type="submit"
+                      value="Buscar"
+                      className="btn btn-primary btn-block"
+                    />
+                  </div>
+
                 </div>
-                <input type="submit" value="Buscar" className="btn btn-primary btn-block"/>
               </form>
+              {/* Muestra la ficha del alumno y el boton para solicitar el prestamo */}
+              {fichaAlumno}
+              {btnSolicitar}
             </div>
           </div>
         </div>
